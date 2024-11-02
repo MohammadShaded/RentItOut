b60f8dd3c56819758935e7c5d835b2e41f0c0b39
Baha-Alawneh <bahaalawneh07@gmail.com>
2024-11-02 11:50:34 +0200

    solve some conflicts

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import './config/db.js'; // Ensure db.js is loaded after dotenv.config()
+import "./config/db.js"; // Ensure db.js is loaded after dotenv.config()
 
-import userRoutes from './routes/userRoutes.js';
-
+import itemRouters from "./routes/item.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 app.use(express.json());
 
-
 //note for the team: under here you have to add your routes usings
-app.use('/users', userRoutes);
-
+app.use("/users", userRoutes);
+app.use("/items", itemRouters);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
