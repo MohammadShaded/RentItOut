16fbdf3058641f0e9f70d55e5810af7053ae16ec
MohammadShadid <3sfr3sfr@gmail.com>
2024-11-02 16:58:22 +0200

    Remove ignored files from Git tracking

--- models/User.js
+++ models/User.js
@@ -1,14 +1,24 @@
-const db = require('../config/db.js');
+import pool from '../config/db.js';
+import { v4 as uuidv4 } from 'uuid';
 
-
-const User ={
-
-    createUser:async (data) =>{
-        
+const User = {
+    // Method to create a new user
+    createUser: async (data) => {
+        const userId = uuidv4(); // Generate UUID for user ID
+        const { name, email, password, role, phone_number, location_id } = data;
+        const query = `INSERT INTO User (user_id, name, email, password, role, phone_number, location_id) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+        const [result] = await pool.query(query, [userId, name, email, password, role, phone_number, location_id]);
+        return userId; // Returns the generated UUID of the newly created user
     },
 
+    // Method to find a user by email
+    findUserByEmail: async (email) => {
+        const query = 'SELECT * FROM User WHERE email = ?';
+        const [rows] = await pool.query(query, [email]);
+        return rows[0]; // Returns the user record if found, otherwise undefined
+    },
 
+    // Other model methods as needed...
+};
 
-
-}
 export default User;
