6e8cde6ee6fa9c42c8a286069cf82fba5553a0a7
nassarharashi <nassark669@gmail.com>
2024-11-04 16:30:04 +0200

    create new review

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -5,10 +5,8 @@ import { isBlacklisted } from '../blacklist.js';
 dotenv.config();
 
 const authenticateToken = (req, res, next) => {
-    // Get the token from the Authorization header
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Extract the token after "Bearer"
-
+    const token = authHeader && authHeader.split(' ')[1]; 
     if (!token) {
         // If no token is provided, deny access
         return res.status(401).json({ message: 'Access denied. No token provided.' });
@@ -26,10 +24,10 @@ const authenticateToken = (req, res, next) => {
         }
 
         // If the token is valid, add user info to the request object
-        
+
         req.user = {
-            user_id: user.id,  
-            role: user.role    
+            user_id: user.id,
+            role: user.role
         }
         next(); // Move to the next middleware or route handler
     });
